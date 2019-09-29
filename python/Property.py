50d649d01199e731f5edb6051ff40ca25c24411b
sathishkumar R <sathishkumar.r@dhl.com>
2019-09-30 00:06:11 +0530

    Property

---
+++ python/Property.py
@@ -0,0 +1,35 @@
+class Person():
+
+    def __init__(self, firstname, lastname):
+        self.first = firstname
+        self.last = lastname
+
+    @property
+    def fullname(self):
+        return self.first + ' ' + self.last
+
+    @fullname.setter
+    def fullname(self, name):
+        firstname, lastname = name.split()
+        self.first = firstname
+        self.last = lastname
+
+    @fullname.deleter
+    def fullname(self):
+        self.first = None
+        self.last = None
+
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+
+# Init a Person
+person = Person('sathish', 'kumar')
+print(person.fullname)  # > selva prabhakaran
+
+# Deleting fullname calls the deleter method, which erases self.first and self.last
+del person.fullname
+
+# Print the changed values of `first` and `last`
+print(person.first)  # > None
+print(person.last)  # > None
